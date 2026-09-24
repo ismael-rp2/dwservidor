b5ef4b3ca3f5bc6fb532016921439a1dbfedc49e
Ismael <ismreyper@alu.edu.gva.es>
2026-09-24 19:32:31 +0200

    Para Claudia y Diego

--- proyecto1/src/index.php
+++ proyecto1/src/index.php
@@ -1,4 +1,19 @@
 <?php
 
-include "app/Views/backend/admin.index.html";
-echo "HOla";
+    //$titulo = $_SERVER['REQUEST_RI'];
+
+    include_once "./vendor/autoload.php";
+
+    use Phroute\Phroute\RouteCollector;
+
+    $router = new RouteCollector();
+    $router->get('/admin', function(){
+        return 'Estas intentando acceder a la funcion de admin';
+    });
+
+    $dispatcher = new Phroute\Phroute\Dispatcher($router->getData());
+
+    $response = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+
+    // Print out the value returned from the dispatched function
+    echo $response;
